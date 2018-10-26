1ac4cccabfa5f5550c44b218a740ec7f6d44482e
Apurva N. Saraogi <apurva9@iitg.ernet.in>
2018-10-26 23:54:30 +0530

    Merged App

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <location/LocationPlugin.h>
+#import <map_view/MapViewPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPlugin"]];
+  [MapViewPlugin registerWithRegistrar:[registry registrarForPlugin:@"MapViewPlugin"]];
+}
+
+@end
